62a800040a1865e94829784bea748a94b693ac3b
Rayen Slouma <rayen.slouma@etudiant-fst.utm.tn>
2025-01-17 14:23:32 +0100

    "removes"

--- gems-frontend/src/app/admin-dashboard/admin-dashboard.module.ts
+++ gems-frontend/src/app/admin-dashboard/admin-dashboard.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminDashboardComponent } from './admin-dashboard.component';
 import { UserListComponent } from './user-list/user-list.component'; // Import UserListComponent
-import { EventListComponent } from './event-list/event-list.component'; // Import EventListComponent
 import { RouterModule } from '@angular/router';
 import { AdminEventListComponent } from './admin-event-list/admin-event-list.component';
 
@@ -10,7 +9,7 @@ import { AdminEventListComponent } from './admin-event-list/admin-event-list.com
   declarations: [
     AdminDashboardComponent,
     UserListComponent,   // Add UserListComponent here if you have created it
-    EventListComponent, AdminEventListComponent,  // Add EventListComponent here if you have created it
+    AdminEventListComponent,  // Add EventListComponent here if you have created it
   ],
   imports: [
     CommonModule,
